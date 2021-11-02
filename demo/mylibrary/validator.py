abee1ec11351900eab782274c0a310585c57f311
HasmH <h.hasam@outlook.com>
2021-11-02 22:02:29 +1100

    Made some progress on schema, however hitting a road block in capabilities of yaml validation

--- demo/mylibrary/validator.py
+++ demo/mylibrary/validator.py
@@ -1,10 +1,30 @@
-import yamale
-#Please ensure to run the following command 
-#'pip3 install yamale' (since we don't have virtual environments)
-def validate_rules(rules):
-    # Create a Schema object - ensure it is in the root of library's folder
-    schema = yamale.make_schema('./schema.yaml')
-    # Create a Data object - parses user-defined rules to yamale parser
-    data = yamale.make_date(rules)
-    # Validate data against the schema. Throws a ValueError if data is invalid.
-    yamale.validate(schema, data)
+import yaml
+import os
+from cerberus import Validator
+
+
+#Refer to https://docs.python-cerberus.org/en/stable/customize.html#validator-schema
+#Used this to create custom validator methods 
+class overWatchValidator(Validator):
+    def foo():
+        return 0
+
+#Taken from: https://stackoverflow.com/questions/1724693/find-a-file-in-python
+#Used to find path of 'rules.yaml' file in the root of user's project directory. 
+#Intelligently does this (no hardocded paths), thus, it does not matter whether user is on Windows or Linux
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+def load_rules(): 
+    with open(str(find('rules.yaml', os.path.abspath(os.curdir)))) as f:
+        rules = yaml.load(f, Loader=yaml.FullLoader)
+        return rules 
+
+schema = eval(open('demo\mylibrary\schema.py', 'r').read()) 
+v = Validator(schema)
+rules = load_rules()
+print(v.validate(rules, schema))
+print(v.errors)
+
