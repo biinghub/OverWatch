218ce5cd901639056d2ae89d17f3a426961d8707
Kyu-Sang Kim <kyusangkim99@gmail.com>
2021-11-13 22:35:48 +1100

    updated CDK + validator fixed to actually validate

--- demo/9447demoweb.py
+++ demo/9447demoweb.py
@@ -9,27 +9,27 @@ PRESET_2 = "database_query, sensitive"
 PRESET_3 = "financial_data, PII"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
 
         data = request.form
-        if data['query'] == 'query':
+        if data["query"] == "query":
             # Example 1: Developer can just specify the certain events
-            actions.monitor_event("critical", "user_event", tags=[
-                                  "database_query", "sensitive", "PII"])
+            actions.monitor_event(
+                "critical", "user_event", tags=["database_query", "sensitive", "PII"]
+            )
             # Example 2: Developer can use a common preset
             actions.monitor_event("major", "user_event", tags=[PRESET_1])
             # Example 3: Developer can combine presets
-            actions.monitor_event("minor", "machine_event",
-                                  tags=[PRESET_2, PRESET_3])
+            actions.monitor_event("minor", "machine_event", tags=[PRESET_2, PRESET_3])
 
-            return render_template('success.html')
+            return render_template("success.html")
         else:
-            return render_template('error.html')
+            return render_template("error.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=6969, debug=True)
--- ow-pipeline-cdk/app.py
+++ ow-pipeline-cdk/app.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python3
 
-from aws_cdk.core import App, Construct, Stack
+from aws_cdk.core import App, Construct, Stack, RemovalPolicy
 import aws_cdk.aws_codebuild as codebuild
 import aws_cdk.aws_iam as iam
+import aws_cdk.aws_s3 as s3
 
-# S3 Access Policy
-s3AccessPolicy = iam.PolicyStatement(
-    actions=["s3:PutObject", "s3:GetObject", "s3:GetObjectVersion", "s3:List*"],
-    resources=["*"],
-    sid="S3AccessPolicy",
-)
+
+# OW Infrastructure Stack
+class OverWatchInfraStack(Stack):
+    artifactBucket = None  # Artifact Store S3 bucket
+    artifactBucketAccessPolicy = None  # Artifact Store S3 Policy
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        self.artifactBucket = s3.Bucket(
+            self,
+            "OverwatchArtifactBucket",
+            encryption=s3.BucketEncryption.KMS_MANAGED,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+        self.artifactBucketAccessPolicy = iam.PolicyStatement(
+            actions=["s3:PutObject", "s3:GetObject", "s3:GetObjectVersion", "s3:List*"],
+            resources=[
+                self.artifactBucket.bucket_arn + "*"
+            ],  # need the "*" to be able get the source artis properly
+            sid="overwatchArtifactBucketAccessPolicy",
+        )
 
 
 # OW Validation Stack
 class OverWatchValidateStack(Stack):
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+    def __init__(
+        self, scope: Construct, id: str, artifactStack: OverWatchInfraStack, **kwargs
+    ) -> None:
         super().__init__(scope, id, **kwargs)
 
         validate = codebuild.PipelineProject(
@@ -45,17 +65,18 @@ class OverWatchValidateStack(Stack):
             environment=codebuild.BuildEnvironment(
                 build_image=codebuild.LinuxBuildImage.STANDARD_5_0
             ),
-            # TODO: Potentially put overwatch logs into Cloudwatch?
         )
 
         # validate codebuild project has the permissions to get S3 objects (for codepipeline)
-        # global s3 bucket access
-        validate.add_to_role_policy(s3AccessPolicy)
+        # artifact store s3 bucket access
+        validate.add_to_role_policy(artifactStack.artifactBucketAccessPolicy)
 
 
 # OW Deployment Stack
 class OverWatchDeployStack(Stack):
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+    def __init__(
+        self, scope: Construct, id: str, artifactStack: OverWatchInfraStack, **kwargs
+    ) -> None:
         super().__init__(scope, id, **kwargs)
 
         deploy = codebuild.PipelineProject(
@@ -83,12 +104,11 @@ class OverWatchDeployStack(Stack):
             environment=codebuild.BuildEnvironment(
                 build_image=codebuild.LinuxBuildImage.STANDARD_5_0
             ),
-            # TODO: Potentially put overwatch logs into Cloudwatch?
         )
 
         # deploy codebuild project has the permissions to get S3 objects (for codepipeline)
-        # global s3 bucket access
-        deploy.add_to_role_policy(s3AccessPolicy)
+        # artifact store s3 bucket access
+        deploy.add_to_role_policy(artifactStack.artifactBucketAccessPolicy)
 
 
 # OW Application
@@ -96,13 +116,18 @@ class OverWatchService(Construct):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        owValidateStack = OverWatchValidateStack(self, "ow-validate")
-        owDeployStack = OverWatchDeployStack(self, "ow-deploy")
+        owInfraStack = OverWatchInfraStack(self, "ow-infra")
+        owValidateStack = OverWatchValidateStack(self, "ow-validate", owInfraStack)
+
+        owValidateStack.add_dependency(
+            target=owInfraStack, reason="Require Infrastructure for OverWatch"
+        )
+
+        owDeployStack = OverWatchDeployStack(self, "ow-deploy", owInfraStack)
 
         owDeployStack.add_dependency(
             target=owValidateStack, reason="Require Validation for Deployment"
         )
-        # TODO: Potentially put overwatch logs into Cloudwatch?
 
 
 # Begin OverWatch Service deployment
