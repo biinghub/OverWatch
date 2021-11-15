95ef65a2bbe79c37a066c4ad788ebb4e48738542
Kyu-Sang Kim <kyusangkim99@gmail.com>
2021-11-15 17:33:13 +1100

    fix deployer in regards to list of rules

--- ow-core/deployer/deployer.py
+++ ow-core/deployer/deployer.py
@@ -52,7 +52,7 @@ class OverwatchDeployer:
         for rule in self.rules:
             with open(self.rules_dir_path + rule, "r") as stream, open(DEFAULT_PATH, "r") as default:
                 try:
-                    current_rule = yaml.safe_load(stream)
+                    current_rules = yaml.safe_load(stream)
                     default_rule = yaml.safe_load(default)
                 except yaml.YAMLError as exc:
                     print(exc)
@@ -91,44 +91,47 @@ class OverwatchDeployer:
                 }
 
                 # print(default_rule['Metric'])
+                # create metric filters for all alarms
+                for current_rule in current_rules:
+                    for field, value in list(metric_fields.items()):
+                        if field in current_rule['Metric']:
+                            metric_fields[field] = current_rule['Metric'][field]
+                        else:
+                            metric_fields[field] = default_rule['Metric'][field]
+                            if metric_fields[field] == "" or metric_fields[field] == []:
+                                del metric_fields[field]
 
-                for field, value in list(metric_fields.items()):
-                    if field in current_rule['Metric']:
-                        metric_fields[field] = current_rule['Metric'][field]
-                    else:
-                        metric_fields[field] = default_rule['Metric'][field]
-                        if metric_fields[field] == "" or metric_fields[field] == []:
-                            del metric_fields[field]
+                    # print(metric_fields)
 
-                # print(metric_fields)
+                    try:
+                        # Creating metric filter
+                        logs.put_metric_filter(**metric_fields)
+                    except Exception as err:
+                        print("something went wrong creating metric filter")
+                        exit(1)
 
-                try:
-                    # Creating metric filter
-                    logs.put_metric_filter(**metric_fields)
-                except Exception as err:
-                    print("something went wrong creating metric filter")
-                    exit(1)
+                # deploy alarms
+                for current_rule in current_rules:
+                    for field, value in list(alarm_fields.items()):
+                        if field in current_rule['Alarm']:
+                            alarm_fields[field] = current_rule['Alarm'][field]
+                        else:
+                            alarm_fields[field] = default_rule['Alarm'][field]
+                            if alarm_fields["MetricName"] == "":
+                                alarm_fields["MetricName"] = metric_fields["metricTransformations"][0]["metricName"]
+                            if alarm_fields["Namespace"] == "":
+                                alarm_fields["Namespace"] = metric_fields["metricTransformations"][0]["metricNamespace"]
+                            elif alarm_fields[field] == "" or alarm_fields[field] == []:
+                                del alarm_fields[field]
 
-                for field, value in list(alarm_fields.items()):
-                    if field in current_rule['Alarm']:
-                        alarm_fields[field] = current_rule['Alarm'][field]
-                    else:
-                        alarm_fields[field] = default_rule['Alarm'][field]
-                        if alarm_fields["MetricName"] == "":
-                            alarm_fields["MetricName"] = metric_fields["metricTransformations"][0]["metricName"]
-                        if alarm_fields["Namespace"] == "":
-                            alarm_fields["Namespace"] = metric_fields["metricTransformations"][0]["metricNamespace"]
-                        elif alarm_fields[field] == "" or alarm_fields[field] == []:
-                            del alarm_fields[field]
+                    # print(alarm_fields)
 
-                # print(alarm_fields)
-
-                try:
-                    # Creating CloudWatch Alarm
-                    cloudwatch.put_metric_alarm(**alarm_fields)
-                except Exception as err:
-                    print("something went wrong creating CloudWatch Alarm")
-                    exit(1)
+                    try:
+                        # Creating CloudWatch Alarm
+                        cloudwatch.put_metric_alarm(**alarm_fields)
+                    except Exception as err:
+                        print("something went wrong creating CloudWatch Alarm")
+                        exit(1)
 
     def deploy(self):
         self.load_rules()
