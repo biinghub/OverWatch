1c24f71c8fe4312df521960eb6bc046fe44de902
HasmH <h.hasam@outlook.com>
2021-11-15 18:40:17 +1100

    Fixed tests to work with Kims refactor, all tests pass now

--- ow-core/validator/tests/test_validator.py
+++ ow-core/validator/tests/test_validator.py
@@ -1,56 +1,63 @@
 import pytest
 from validator import validator
 from validator.validator import DuplicateNameException
+from validator.validator import ValidationException
 import unittest
 
 class ValidationTestCases(unittest.TestCase):
     def test_set_rules(self):
         #The test ensures argument parsing for rule declaration works
-        v1 = validator.OverwatchValidator('example/rules.yaml', False)
-        v2 = validator.OverwatchValidator('ow-core/validator/tests/test.yaml', False)
-        assert v1.rule_path == 'example/rules.yaml'
-        assert v2.rule_path == 'ow-core/validator/tests/test.yaml'
+        v1 = validator.OverwatchValidator('ow-core/example', False)
+        v2 = validator.OverwatchValidator('ow-core/validator/tests/test2', False)
+        assert v1.rules_dir_path == 'ow-core/example/'
+        assert v2.rules_dir_path == 'ow-core/validator/tests/test2/'
 
     def test_validate_rules_structure(self):
-        v2 = validator.OverwatchValidator('example/rules.yaml', False)
+        v2 = validator.OverwatchValidator('ow-core/validator/tests/test2', False)
+        v2.load_rules()
         #rules.yaml is an example configuration file that follows the schema
         #defined in schema.yaml
         #therefore, the assertion proves that the validate rules structure function works
         #by returning true
-        assert v2.validate_rules_structure() == (True, "Valid Rules File")
+        assert v2.validate_rules_structure() == (True, "All Rules Files Valid")
 
     def test_validate_rules_structure_fails(self):
-        v3 = validator.OverwatchValidator('ow-core/validator/tests/test.yaml', False)
+        v3 = validator.OverwatchValidator('ow-core/validator/tests/test1', False)
+        v3.load_rules()
         #test.yaml is an example configuration file that does NOT follow the schema
         #defined in schema.yaml
         #therefore, the assertion proves that the validate rules structure function works
-        #by returning false
-        assert v3.validate_rules_structure() == (False, "Invalid Rules File")
+        #by raising exception
+        assert v3.validate_rules_structure() == (False, "Invalid Rules File - more_bad_rule.yaml")
 
     def test_validate_alarm_attributes_works(self):
-        v2 = validator.OverwatchValidator('example/rules.yaml', False)
+        v2 = validator.OverwatchValidator('ow-core/example/', False)
+        v2.load_rules()
         #rules.yaml only has 1 alarm name, called alarmnamestring
         #assertion shows the function works in grabbing these attribtues
-        assert v2.validate_alarm_attributes() == ['alarmnamestring']
+        assert v2.validate_alarm_attributes() == ['alarmnamestring', 'simple-rule']
 
     def test_validate_alarm_attributes_exception(self):
-        v2 = validator.OverwatchValidator('ow-core/validator/tests/test2.yaml', False)
+        v2 = validator.OverwatchValidator('ow-core/validator/tests/test2', False)
+        v2.load_rules()
         #rules.yaml only has 1 alarm name, called alarmnamestring
         #assertion shows the function works in grabbing these attribtues
-        with self.assertRaises(DuplicateNameException):
+        with self.assertRaises(ValidationException):
             v2.validate_alarm_attributes()
 
     def test_validate_metric_attributes(self):
-        v2 = validator.OverwatchValidator('example/rules.yaml', False)
+        v2 = validator.OverwatchValidator('ow-core/example', False)
+        v2.load_rules()
         #rules.yaml only has 1 alarm name, called alarmnamestring
         #assertion shows the function works in grabbing these attribtues
-        assert v2.validate_metric_attributes() == ['FilterName']
+        assert v2.validate_metric_attributes() == ['FilterName', 'simple-filter']
 
     def test_validate_metric_attributes_exception(self):
-        v2 = validator.OverwatchValidator('ow-core/validator/tests/test2.yaml', False)
+        v2 = validator.OverwatchValidator('ow-core/validator/tests/test2', False)
+        v2.load_rules()
         #rules.yaml only has 1 alarm name, called alarmnamestring
         #assertion shows the function works in grabbing these attribtues
-        with self.assertRaises(DuplicateNameException):
+        with self.assertRaises(ValidationException):
             v2.validate_metric_attributes()
 
 if __name__ == '__main__':
